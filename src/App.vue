f4adaf97e9d17a4408ed405b23addbc97e236804
Julius Guevarra <juliusguevarra101@gmail.com>
2019-03-05 22:23:01 +0800

    Add task method

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
-  <div>
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div class="todo">
+    <input @change="onInputChange" type="text" v-model="inputValue">
+    <button @click="addTask">Add Task</button>
+    <template v-for="todo in tasks">
+      <TodoItem :todo="todo" :key="todo.id" />
+    </template>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import moment from 'moment'
+import uuid from 'uuid'
+
+import TodoItem from './components/TodoItem'
 
 export default {
   name: 'App',
+  data: function () {
+    return {
+      inputValue: '',
+      tasks: []
+    }
+  },
+  methods: {
+    onInputChange (e) {
+      const val = e.target.value
+      this.inputValue = val
+    },
+    addTask () {
+      this.tasks = [
+        ...this.tasks,
+        {
+          id: uuid(),
+          createdAt: moment().valueOf(),
+          task: this.inputValue,
+          status: 'unfinished' 
+        }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    TodoItem
   }
 }
 </script>
@@ -21,8 +50,11 @@ div {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.todo {
+  padding: 4rem 2rem;
+}
+
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="todo__item"> 
+    <h1>{{ todo.task }}</h1>
+    <span>Created at: {{ createdAt(todo.createdAt) }}</span>
+    <span>Status: {{ todo.status }}</span>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  name: 'TodoItem',
+  methods: {
+    createdAt (date) {
+      return moment(date).format('LL')
+    }
+  },
+  props: {
+    todo: {
+      task: String,
+      createdAt: Number,
+      id: String,
+      status: String
+    }
+  }
+}
+</script>
